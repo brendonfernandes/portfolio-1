5ab03ba8cfc4841221503dcb2b4265ede1a68b51
Brendon Fernandes <brendon.fernandes@fcincorporadora.com.br>
2023-02-27 20:12:58 -0300

    primeiro-commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './header.css'
+const Header = () => {
+  return (
+    <div>
+
+        <Header>
+            <div className="container header__container">
+                <h5> Olá Eu sou</h5>
+                <h1>Brendon fernandes</h1>
+                <h5 className="text-ligth">Desenvolvedor Front end<br>
+                Cursando Engenharia de software</br></h5>
+            </div>
+        </Header>
+
+    </div>
+  )
+}
+
+export default Header
